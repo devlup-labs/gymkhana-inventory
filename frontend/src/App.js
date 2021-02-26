3f7d34691a0bc618ac22e4652cc9aadbd717b4bf
Suyash Agnihotri <suyash1304@gmail.com>
2021-02-26 14:17:07 +0530

    Added Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import PrimarySearchAppBar from './components/navbar';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+          <div className="header">
+            <PrimarySearchAppBar/>
+          </div>
+          
+          <div className="router-div">
+            {/* routers */}
+          </div>
+          
+          
+          <div className="footer">
+            <h1>Footer</h1>
+          </div>
+        </div>
+      
   );
 }
 
