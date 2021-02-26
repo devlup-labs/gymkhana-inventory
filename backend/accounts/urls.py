1bd80fd671ce3d62551d43784016bcf84af1fbb3
Apple <savanihard2001@gmail.com>
2021-02-26 18:38:06 +0530

    Added user edit profile feature

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import GoogleLogin
+from .views import GoogleLogin,EditUserProfile
 
 urlpatterns = [
 
     path('login/google/', GoogleLogin.as_view(), name='google_login'),
+    path('editprofile/', EditUserProfile.as_view(), name="edit_profile"),
 ]
