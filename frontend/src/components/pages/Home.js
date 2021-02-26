f2f60f4bbc4f172ffb82d7f4d8462d49f7275eb5
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2021-02-26 12:41:22 +0530

    active_folder_structure_created

---
+++ frontend/src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+
+        <div>
+            This is Home Page
+        </div>
+
+    )
+}
+
+export default Home;
---
+++ frontend/src/components/pages/SearchResult.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SearchResult() {
+    return (
+
+        <div>
+            This is SearchResult Page
+        </div>
+
+    )
+}
+
+export default SearchResult;
