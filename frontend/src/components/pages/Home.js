06a7816ed7f5ae067a587fe0a84b3934170ba6dc
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2021-02-27 02:17:22 +0530

    eslint_and_prettier_added

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
 import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
@@ -10,107 +10,121 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import IITJ from '../../IITJ.png'
+import IITJ from '../../IITJ.png';
 import { Link } from 'react-router-dom';
 
-
 const useStyles = makeStyles((theme) => ({
- Searchbutton: {
-    padding:"10px",
-    marginBottom:"3px"
+    Searchbutton: {
+        padding: '10px',
+        marginBottom: '3px'
     },
-  Gridcontainer:{
-    marginTop:"5px"
-  }  
+    Gridcontainer: {
+        marginTop: '5px'
+    }
+}));
 
-  }
-));
-
- function Home(){
-
+function Home() {
     //  -------Fake Data -------
 
-    const data= [
-      { 
-        id:1,
-        name: "Product 1",
-        description: "A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.",
-      },
-      { 
-        id:2,
-        name: "Product 2",
-        description: "A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.",
-      },
-      { 
-        id:3,
-        name: "Product 3",
-        description: "A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.",
-      },
-      { 
-        id:4,
-        name: "Product 4",
-        description: "A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.",
-      }
-    ]
+    const data = [
+        {
+            id: 1,
+            name: 'Product 1',
+            description:
+                'A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.'
+        },
+        {
+            id: 2,
+            name: 'Product 2',
+            description:
+                'A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.'
+        },
+        {
+            id: 3,
+            name: 'Product 3',
+            description:
+                'A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.'
+        },
+        {
+            id: 4,
+            name: 'Product 4',
+            description:
+                'A paragraph is a self-contained unit of discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.'
+        }
+    ];
 
     // ---------xxxx--------
-     const classes = useStyles();
+    const classes = useStyles();
     return (
-      <div className="container home">
-        <div className="search-area">
-          <div className="texts">
-            <h2 id="heading">Search for Equipments</h2>
-            <p>Cameras, Microphone, camcoders everything at on place</p>
-          </div>
-          <div className="search-bar">
-            <form>
-          <input classname="bar" type="text" placeholder="Search for equipments"/>
-          <IconButton type="submit" aria-label="search" className={classes.Searchbutton}>
-              <SearchIcon />
-          </IconButton>
-          </form>
-          </div>\
-          </div>
-          <Grid container spacing={3}>
-         {data.map(item => {
-            return (
-              <Grid item xs={12} sm={6} md={3} className={classes.Gridcontainer}>
-              <Card className={classes.root} key={item.id}>
-              <CardActionArea>
-                  <CardMedia
-                  component="img"
-                  alt="Contemplative Reptile"
-                  height="140"
-                  image={IITJ}
-                  title="Contemplative Reptile"
-                  />
-                  <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                      {item.name}
-                  </Typography>
-                  <Typography variant="body2" color="textSecondary" component="p">
-                      {item.description}
-                  </Typography>
-                  </CardContent>
-              </CardActionArea>
-              <CardActions>
-                  <Button  component={ Link} to="/itempage" size="small" color="primary">
-                  Item Page
-                  </Button>
-                  <Button size="small" color="primary">
-                  Request
-                  </Button>
-              </CardActions>
-          </Card>
-          </Grid>
-            )
-         })}
-       
-       </Grid>
-        
-      </div>
+        <div className="container home">
+            <div className="search-area">
+                <div className="texts">
+                    <h2 id="heading">Search for Equipments</h2>
+                    <p>Cameras, Microphone, camcoders everything at on place</p>
+                </div>
+                <div className="search-bar">
+                    <form>
+                        <input className="bar" type="text" placeholder="Search for equipments" />
+                        <IconButton
+                            type="submit"
+                            aria-label="search"
+                            className={classes.Searchbutton}>
+                            <SearchIcon />
+                        </IconButton>
+                    </form>
+                </div>
+                \
+            </div>
+            <Grid container spacing={3}>
+                {data.map((item) => {
+                    return (
+                        <Grid
+                            item
+                            key={item.id}
+                            xs={12}
+                            sm={6}
+                            md={3}
+                            className={classes.Gridcontainer}>
+                            <Card className={classes.root}>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        alt="Contemplative Reptile"
+                                        height="140"
+                                        image={IITJ}
+                                        title="Contemplative Reptile"
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            {item.name}
+                                        </Typography>
+                                        <Typography
+                                            variant="body2"
+                                            color="textSecondary"
+                                            component="p">
+                                            {item.description}
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                <CardActions>
+                                    <Button
+                                        component={Link}
+                                        to="/itempage"
+                                        size="small"
+                                        color="primary">
+                                        Item Page
+                                    </Button>
+                                    <Button size="small" color="primary">
+                                        Request
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    );
+                })}
+            </Grid>
+        </div>
     );
-  }
-
+}
 
 export default Home;
--- frontend/src/components/profiles/UserProfile.js
+++ frontend/src/components/profiles/UserProfile.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
 function UserProfile() {
-    return (
-
-        <div>
-            This is UserProfile Page
-        </div>
-
-    )
+    return <div>This is UserProfile Page</div>;
 }
 
 export default UserProfile;
