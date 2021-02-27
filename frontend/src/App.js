7a70cb66692feac9dfa0619f09aee11d2bbf6780
Anshul Tilondiya <43814313+Anshultilondiya@users.noreply.github.com>
2021-02-27 21:58:21 +0530

    Revert "updated itempage.js file and added route of the same"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import "./App.css";
-import PrimarySearchAppBar from "./components/navbar";
-import { BrowserRouter } from "react-router-dom";
-import Router from "./Router";
-import Footer from "./components/footer";
+import './App.css';
+import PrimarySearchAppBar from './components/navbar';
+import { BrowserRouter} from 'react-router-dom';
+import Router from './Router';
+import Footer from './components/footer';
+
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className="container">
-        <div className="header">
-          <PrimarySearchAppBar />
+          <div className="header">
+            <PrimarySearchAppBar/>
+          </div>
+          
+          <div className="router-div">
+            <Router/>
+          </div>
+          
+          
+          <div className="footer">
+            <Footer/>
+          </div>
         </div>
-
-        <div className="router-div">
-          <Router />
-        </div>
-
-        <div className="footer">
-          <Footer />
-        </div>
-      </div>
     </BrowserRouter>
   );
 }
