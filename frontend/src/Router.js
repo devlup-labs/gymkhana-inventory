7ac4b4b4d8e61fb54ae293765fd902226cbf0eae
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2021-02-27 16:44:00 +0530

    Modal_page_added

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -3,13 +3,14 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import UserProfile from './components/profiles/UserProfile';
 import ItemsPage from './components/pages/ItemsPage';
-
+import Modal from './components/pages/Modal';
 
 const Router = () => {
     return (
         <Switch>
           <Route path="/" exact component={Home}/>
-          <Route path="/profile" component={UserProfile}/>
+        <Route path="/profile" component={UserProfile} />
+        <Route path="/modal" component={Modal}/>
           {/* <Route path="/logout" component={Logout}/> */}
           {/* <Route path="/notification" component={Notification}/> */}
           <Route path="/itempage" component={ItemsPage}/>
--- frontend/src/components/pages/Modal.js
+++ frontend/src/components/pages/Modal.js
@@ -1,13 +1,95 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import PublishSharpIcon from '@material-ui/icons/PublishSharp';
+import { Input } from '@material-ui/core';
 
-function Modal() {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        color: theme.palette.text.primary
+    },
+    textField: {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: '25ch'
+    }
+}));
+
+export default function LayoutTextFields() {
+    const classes = useStyles();
+
     return (
-
-        <div>
-            This is Modal Page
+        <div className={classes.root}>
+            <div>
+                <h1>Adding equipment to Respective Society</h1>
+                <TextField
+                    id="outlined-full-width"
+                    label="Product Name"
+                    style={{ margin: 8 }}
+                    placeholder="Enter Your Product Here"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true
+                    }}
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-full-width"
+                    label="Model"
+                    style={{ margin: 8 }}
+                    placeholder="Enter Model of Your Product Here"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true
+                    }}
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-full-width"
+                    label="Description"
+                    style={{ margin: 8 }}
+                    placeholder="Description"
+                    fullWidth
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true
+                    }}
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-full-width"
+                    label="Quantity"
+                    style={{ margin: 8 }}
+                    placeholder="Quantity"
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true
+                    }}
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-full-width"
+                    label="Type"
+                    style={{ margin: 8 }}
+                    placeholder="Type"
+                    InputLabelProps={{
+                        shrink: true
+                    }}
+                    variant="outlined"
+                />
+                <br />
+                <Input
+                    type="file"
+                    style={{ margin: 10 }}
+                    label="Upload file"
+                    large
+                    startAdornment={<PublishSharpIcon fontSize="large" />}
+                />
+            </div>
         </div>
-
-    )
+    );
 }
-
-export default Modal;
