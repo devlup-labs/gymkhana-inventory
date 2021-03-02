ca35f04d00ebfd058e43a5bfbab2ff3a241cbd0c
Suyash Agnihotri <suyash1304@gmail.com>
2021-03-02 15:57:35 +0530

    Improved Padding , Margin and Colors

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -19,8 +19,7 @@ const useStyles = makeStyles((theme) => ({
         marginBottom: '3px'
     },
     Gridcontainer: {
-        marginTop: '5px',
-        padding: '5px'
+        marginTop: '50px'
     }
 }));
 
@@ -78,81 +77,102 @@ function Home() {
             <div className="sep">
                 <i>Every moment is a fresh beginning. – T.S Eliot</i>
             </div>
-            <Grid container>
-                {data.map((item) => {
-                    return (
-                        <Grid
-                            key={item.id}
-                            item
-                            xs={12}
-                            sm={6}
-                            md={3}
-                            className={classes.Gridcontainer}>
-                            <Card className={classes.root}>
-                                <CardActionArea>
-                                    <CardMedia
-                                        component="img"
-                                        alt="Contemplative Reptile"
-                                        height="140"
-                                        image={IITJ}
-                                        title="Contemplative Reptile"
-                                    />
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            {item.name}
-                                        </Typography>
-                                        <Typography
-                                            variant="body2"
-                                            color="textSecondary"
-                                            component="p">
-                                            {item.description}
-                                        </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                                <CardActions>
-                                    <Button
-                                        component={Link}
-                                        to="/itempage"
-                                        size="small"
-                                        color="primary">
-                                        Item Page
-                                    </Button>
-                                    <Button size="small" color="primary">
-                                        Request
-                                    </Button>
-                                </CardActions>
-                            </Card>
-                        </Grid>
-                    );
-                })}
-            </Grid>
+            <div className="Grid-container">
+                <Grid container spacing={7} className={classes.Grid}>
+                    {data.map((item) => {
+                        return (
+                            <Grid
+                                key={item.id}
+                                item
+                                xs={12}
+                                sm={6}
+                                md={4}
+                                className={classes.Gridcontainer}>
+                                <Card className={classes.root}>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            component="img"
+                                            alt="Contemplative Reptile"
+                                            height="140"
+                                            image={IITJ}
+                                            title="Contemplative Reptile"
+                                        />
+                                        <CardContent>
+                                            <Typography gutterBottom variant="h5" component="h2">
+                                                {item.name}
+                                            </Typography>
+                                            <Typography
+                                                variant="body2"
+                                                color="textSecondary"
+                                                component="p">
+                                                {item.description}
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                    <CardActions>
+                                        <Button
+                                            component={Link}
+                                            to="/itempage"
+                                            size="small"
+                                            color="primary">
+                                            Item Page
+                                        </Button>
+                                        <Button size="small" color="primary">
+                                            Request
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        );
+                    })}
+                </Grid>
+            </div>
             <div className="separator">
                 <div className="septext">View more</div>
             </div>
             <div className="quotes">
                 <h3>
-                    <i>“Believe you can and you’re halfway there.” -Theodore Roosevelt</i>
+                    <i>
+                        &ldquo;It matters little how much equipment we use; it matters much that we
+                        be masters of all we do use&ldquo;
+                    </i>
                 </h3>
             </div>
             <div className="rules">
-                <h3>Rules to Follow</h3>
-                <p>
+                <h3>Gymkhana Equipment Lending Policies</h3>
+                <p id="lorem">
                     <i>
-                        Contrary to popular belief, Lorem Ipsum is not simply random text. It has
-                        roots in a piece of classical Latin literature from 45 BC, making it over
-                        2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
-                        College in Virginia, looked up one of the more obscure Latin words,
-                        consectetur, from a Lorem Ipsum passage, and going through the cites of the
-                        word in classical literature, discovered the undoubtable source. Lorem Ipsum
-                        comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum
-                        (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a
-                        treatise on the theory of ethics, very popular during the Renaissance. The
-                        first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line
-                        in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s
-                        is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from
-                        de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact
-                        original form, accompanied by English versions from the 1914 translation by
-                        H. Rackham.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit anim id est
+                        laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                        est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                        est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                        est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+                        est laborum.
                     </i>
                 </p>
             </div>
