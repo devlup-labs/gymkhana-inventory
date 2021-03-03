778109a4dbfbb742ff64074fae2816c54a4fb034
Bhawna Chopra <chopra.bhawna1212@gmail.com>
2021-03-03 23:53:54 +0530

    Added flake8

--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,5 +1,4 @@
 from .serializers import EquipmentSerializer
-from django.shortcuts import render
 import json
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
@@ -10,34 +9,38 @@ from rest_framework.decorators import api_view
 # Create your views here.
 from .models import Society, Equipment
 # Create your views here.
+
+
 @api_view(["GET"])
 @csrf_exempt
 def get_equipment_by_society(request):
     payload = json.loads(request.body)
-    society=Society.objects.get(name=payload["society"])
+    society = Society.objects.get(name=payload["society"])
     equip = Equipment.objects.filter(societyname=society)
     serializer = EquipmentSerializer(equip, many=True)
     return JsonResponse({'equipments': serializer.data}, safe=False, status=status.HTTP_200_OK)
+
 
 @api_view(["POST"])
 @csrf_exempt
 def add_equipment(request):
     payload = json.loads(request.body)
     try:
-        society=Society.objects.get(name=payload["society"])
-        equipment=Equipment.objects.create(
+        society = Society.objects.get(name=payload["society"])
+        equipment = Equipment.objects.create(
             name=payload["name"],
             description=payload["description"],
             quantity=payload["quantity"],
             societyname=society,
             numavail=payload["numavail"]
         )
-        serializer=EquipmentSerializer(equipment)
+        serializer = EquipmentSerializer(equipment)
         return JsonResponse({'equipment': serializer.data}, safe=False, status=status.HTTP_201_CREATED)
     except ObjectDoesNotExist as e:
         return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
     except Exception:
         return JsonResponse({'error': 'Something terrible went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 @api_view(["PUT"])
 @csrf_exempt
@@ -53,6 +56,7 @@ def update_equipment(request, id):
         return JsonResponse({'error': str(e)}, safe=False, status=status.HTTP_404_NOT_FOUND)
     except Exception:
         return JsonResponse({'error': 'Something terrible went wrong'}, safe=False, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 @api_view(["DELETE"])
 @csrf_exempt
