3d8051183afc1b37d5d6f482d0b2c213e7e81af9
Nimit3-droid <Nimit3-droid@users.noreply.github.com>
2021-03-05 11:02:40 +0530

    Admin Profile design updated

--- frontend/src/components/profiles/adminProfile/Item.js
+++ frontend/src/components/profiles/adminProfile/Item.js
@@ -1,36 +1,64 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Box, Button } from '@material-ui/core';
+import { Button, Grid, Divider } from '@material-ui/core';
+import camera from './../../../camera.jpg';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+    design: {
+        margin: '4px',
+        padding: '5px'
+    },
+    color1: {
+        color: 'red',
+        fontStyle: 'italic',
+        fontWeight: 'bold'
+    },
+    itemStyle: {
+        fontWeight: 700,
+        fontSize: 20
+    }
+}));
 
 function Item(props) {
     const isClicked = true;
+    const classes = useStyles();
     return (
-        <Box className="cards" style={{ maxwidth: '18rem' }}>
-            <Box className="cardContent">
-                <Box>
-                    <p>Item Name: {props.itemName}</p>
-                    <p>ID: {props.itemId}</p>
-                </Box>
-                <p>Requesting Date: {props.requestingDate}</p>
-                <p>Due Date: {props.dueDate}</p>
-            </Box>
-            {isClicked === true ? (
-                <Box className="buttonAdmin">
-                    <Button variant="contained" color="primary">
-                        Approve
-                    </Button>
-                    <Button variant="contained" color="primary">
-                        Deny
-                    </Button>
-                </Box>
-            ) : null}
-
-            <Box className="cardContent">
-                <p>Request Name: {props.requestaName}</p>
-                <p>Email ID: {props.emailID}</p>
-                <p>Contact NO.: {props.contact}</p>
-            </Box>
-        </Box>
+        <Grid className="cards">
+            <Grid item xs={12} container spacing={2}>
+                <Grid item xs={2} lg={4} xl={6}>
+                    <img src={camera} width="100%" />
+                </Grid>
+                <Divider orientation="vertical" flexItem />
+                <Grid item xs={10} lg={4} spacing={10}>
+                    <Grid item className={`${classes.design} ${classes.itemStyle}`}>
+                        {props.itemName}
+                    </Grid>
+                    <Grid item className={classes.design}>
+                        Equipment ID: {props.itemId}
+                    </Grid>
+                    <Grid item className={`${classes.design} ${classes.color1}`}>
+                        {props.requestaName}
+                    </Grid>
+                    <Grid item className={classes.design}>
+                        {props.emailID}
+                    </Grid>
+                    <Grid item className={classes.design}>
+                        Issues on: {props.requestingDate}
+                    </Grid>
+                    <Grid item className={`${classes.design} ${classes.color1}`}>
+                        Return Date: {props.dueDate}
+                    </Grid>
+                    <Grid item className={`buttonAdmin ${classes.design}`}>
+                        {isClicked === true ? (
+                            <Button variant="contained" color="primary">
+                                View Request
+                            </Button>
+                        ) : null}
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid>
     );
 }
 
--- frontend/src/components/profiles/adminProfile/ManageAccess.js
+++ frontend/src/components/profiles/adminProfile/ManageAccess.js
@@ -1,16 +1,55 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Avatar, Grid, IconButton } from '@material-ui/core';
 import { Delete } from '@material-ui/icons';
 
+const useStyles = makeStyles(() => ({
+    Gridcontainer: {
+        margin: '5px',
+        padding: '10px',
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
+        borderRadius: '9px',
+        justifyContent: 'space-between',
+        width: 'fit-content'
+    },
+    GridcontainerAvtarContent: {
+        display: 'flex',
+        margin: '4px',
+        marginLeft: '10px',
+        marginRight: '10px'
+    },
+    GridcontainerAvtarMP: {
+        margin: '4px',
+        padding: '4px',
+        marginRight: '30px'
+    },
+    GridcontainerAvtar: {
+        width: '250px',
+        height: '250px'
+    }
+}));
+
 function ManageAccess(props) {
+    const classes = useStyles();
+
     return (
-        <Box className="cardContent">
-            <p>Name: {props.name}</p>
-            <p>Email: {props.email}</p>
-            <p>Role: {props.role}</p>
-            <Delete />
-        </Box>
+        <Grid className={classes.Gridcontainer} item container>
+            <Grid className={classes.GridcontainerAvtarContent} item>
+                <Grid className="adminManage">
+                    <Avatar item />
+                </Grid>
+                <Grid className={classes.GridcontainerAvtarMP}>
+                    <Grid item>{props.name}</Grid>
+                    <Grid item>{props.email}</Grid>
+                </Grid>
+            </Grid>
+            <p className="adminTheme">{props.role}</p>
+            <IconButton>
+                <Delete item />
+            </IconButton>
+        </Grid>
     );
 }
 
