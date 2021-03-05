64f24df1f363ea9d137cee6fd21118381891b2b9
AkshayJyani <59910899+AkshayJyani@users.noreply.github.com>
2021-03-05 12:46:17 +0530

    Updated Profile Page and Edit Button
    Profile Page is updated as well as Edit button with functionalities is updated.

--- frontend/src/components/profiles/UserProfile.js
+++ frontend/src/components/profiles/UserProfile.js
@@ -1,189 +1,151 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Grid } from '@material-ui/core';
 
-function UserProfile() {
+function User() {
+    const [contact, setContact] = useState('contact');
+
+    // Donot touch this
+    const [edit, setEdit] = useState('edit');
+    const [save, setSave] = useState('save');
+    console.log(save);
+    function editButton() {
+        if (edit === 'edit') {
+            setEdit('save');
+            setSave('edit');
+        } else {
+            setEdit('edit');
+            setSave('save');
+        }
+    }
+
+    function changeContact(e) {
+        setContact(e.target.value);
+    }
+
     return (
-        <div className="user_profile_page">
-            <div className="user_page">
-                <div className="personal_info">
-                    <img
-                        width="100%"
-                        src="https://cdn1.iconfinder.com/data/icons/technology-devices-2/100/Profile-512.png"
-                    />
-                    <div className="user_contact">
-                        <p>Contact</p>
-                        <p> mo no.</p>
-                        <p>Social (if)</p>
-                        <button className="edit_info_btn">Edit</button>
-                    </div>
-                </div>
-                {/* <div className="vl" /> */}
-                <div className="main_info">
-                    <div className="college_info">
-                        <p>Name of Person</p>
-                        <p>Roll no.</p>
-                        <p>College Address</p>
-                    </div>
-
-                    <div className="active_user_items">
-                        <p>Active</p>
-                        <div className="list_active_user_items_1">
-                            <div className="item_name_info">
-                                <p className="item_name">
-                                    Item_name: <span>Nikkon Cameron</span>
-                                </p>
-                                <p className="item_id">
-                                    Item_Id: <span>5203</span>
-                                </p>
-                            </div>
-                            <div className="date">
-                                <div>
-                                    <p className="issue_date">
-                                        Issue_date:<span>10/02/2020</span>
-                                    </p>
-                                    <p className="due_date">
-                                        Due_date:<span>18/02/2020</span>
-                                    </p>
-                                </div>
-                                <div className="due_date">
-                                    <p className="return_date">
-                                        Return_date:<span>15/02/2020</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="list_active_user_items_1">
-                            <div className="item_name_info">
-                                <p className="item_name">
-                                    Item_name: <span>Nikkon Cameron</span>
-                                </p>
-                                <p className="item_id">
-                                    Item_Id: <span>5203</span>
-                                </p>
-                            </div>
-                            <div className="date">
-                                <div>
-                                    <p className="issue_date">
-                                        Issue_date:<span>10/02/2020</span>
-                                    </p>
-                                    <p className="due_date">
-                                        Due_date:<span>18/02/2020</span>
-                                    </p>
-                                </div>
-                                <div className="due_date">
-                                    <p className="return_date">
-                                        Return_date:<span>15/02/2020</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="list_active_user_items_1">
-                            <div className="item_name_info">
-                                <p className="item_name">
-                                    Item_name: <span>Nikkon Cameron</span>
-                                </p>
-                                <p className="item_id">
-                                    Item_Id: <span>5203</span>
-                                </p>
-                            </div>
-                            <div className="date">
-                                <div>
-                                    <p className="issue_date">
-                                        Issue_date:<span>10/02/2020</span>
-                                    </p>
-                                    <p className="due_date">
-                                        Due_date:<span>18/02/2020</span>
-                                    </p>
-                                </div>
-                                <div className="due_date">
-                                    <p className="return_date">
-                                        Return_date:<span>15/02/2020</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="active_user_items">
-                        <p>Past History</p>
-                        <div className="list_active_user_items_1">
-                            <div className="item_name_info">
-                                <p className="item_name">
-                                    Item_name: <span>Nikkon Cameron</span>
-                                </p>
-                                <p className="item_id">
-                                    Item_Id: <span>5203</span>
-                                </p>
-                            </div>
-                            <div className="date">
-                                <div>
-                                    <p className="issue_date">
-                                        Issue_date:<span>10/02/2020</span>
-                                    </p>
-                                    <p className="due_date">
-                                        Due_date:<span>18/02/2020</span>
-                                    </p>
-                                </div>
-                                <div className="due_date">
-                                    <p className="return_date">
-                                        Return_date:<span>15/02/2020</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="list_active_user_items_1">
-                            <div className="item_name_info">
-                                <p className="item_name">
-                                    Item_name: <span>Nikkon Cameron</span>
-                                </p>
-                                <p className="item_id">
-                                    Item_Id: <span>5203</span>
-                                </p>
-                            </div>
-                            <div className="date">
-                                <div>
-                                    <p className="issue_date">
-                                        Issue_date:<span>10/02/2020</span>
-                                    </p>
-                                    <p className="due_date">
-                                        Due_date:<span>18/02/2020</span>
-                                    </p>
-                                </div>
-                                <div className="due_date">
-                                    <p className="return_date">
-                                        Return_date:<span>15/02/2020</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="list_active_user_items_1">
-                            <div className="item_name_info">
-                                <p className="item_name">
-                                    Item_name: <span>Nikkon Cameron</span>
-                                </p>
-                                <p className="item_id">
-                                    Item_Id: <span>5203</span>
-                                </p>
-                            </div>
-                            <div className="date">
-                                <div>
-                                    <p className="issue_date">
-                                        Issue_date:<span>10/02/2020</span>
-                                    </p>
-                                    <p className="due_date">
-                                        Due_date:<span>18/02/2020</span>
-                                    </p>
-                                </div>
-                                <div className="due_date">
-                                    <p className="return_date">
-                                        Return_date:<span>15/02/2020</span>
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <React.Fragment>
+            <Grid className="user_profile_page">
+                <Grid className="user_profile">
+                    <Grid>
+                        <img
+                            src="https://library.elementor.com/wp-content/uploads/2017/06/3-4.png"
+                            alt="Images"
+                        />
+                    </Grid>
+                    <Grid className="user_information">
+                        <Grid className={save}>
+                            <p className="user_inf">
+                                Name: <span>Elon Musk</span>
+                            </p>
+                            <p className="user_inf">
+                                Email: <span>elonmusk@tesla.com</span>
+                            </p>
+                            <p className="user_inf">
+                                Contact: <span>{contact}</span>
+                            </p>
+                            <p className="user_inf">
+                                Role: <span>Student</span>
+                            </p>
+                        </Grid>
+                    </Grid>
+                    <Grid className={edit}>
+                        <form>
+                            <input
+                                type="text"
+                                placeholder={contact}
+                                onChange={changeContact.bind()}></input>
+                        </form>
+                    </Grid>
+                    <Button className="edit_info_btn" onClick={editButton}>
+                        {edit}
+                    </Button>
+                </Grid>
+                <Grid className="user_transactions">
+                    <Grid className="user_trans_heading">
+                        <span>Active Request</span>
+                    </Grid>
+                    <Grid className="user_active_request">
+                        <Grid className="imges">
+                            <img
+                                src="https://i.pcmag.com/imagery/reviews/05KU5NmyTcDrIOcF9n5F2vz-30..1569479607.jpg"
+                                alt="Photos"
+                            />
+                        </Grid>
+                        <Grid className="user_item_info">
+                            <h2 className="heading">Nikkon Cameron</h2>
+                            <p className="item-details">
+                                Belongs to: <span>Shutterbugs</span>
+                            </p>
+                            <p className="item-details">
+                                Issuees on: <span>30/02/2021</span>
+                            </p>
+                            <p className="item-details">
+                                Description: <span>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem</span>
+                            </p>
+                            <p className="item-details">
+                                <b>
+                                    Deadline: <span>30/02/2022</span>
+                                </b>
+                            </p>
+                            <button type="button">Request Extension</button>
+                        </Grid>
+                    </Grid>
+                    <Grid className="user_active_request">
+                        <Grid className="imges">
+                            <img
+                                src="https://cdn.mos.cms.futurecdn.net/vpfqJBE9TjGpsbjdNBurQX.jpg"
+                                alt="Photos"
+                            />
+                        </Grid>
+                        <Grid className="user_item_info">
+                            <h2 className="heading">Nikkon Cameron</h2>
+                            <p className="item-details">
+                                Belongs to: <span>Shutterbugs</span>
+                            </p>
+                            <p className="item-details">
+                                Issuees on: <span>30/02/2021</span>
+                            </p>
+                            <p className="item-details">
+                                Description: <span>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem</span>
+                            </p>
+                            <p className="item-details">
+                                <b>
+                                    Deadline: <span>30/02/2022</span>
+                                </b>
+                            </p>
+                            <button type="button">Request Extension</button>
+                        </Grid>
+                    </Grid>
+                    <Grid className="user_active_request">
+                        <Grid className="imges">
+                            <img
+                                src="https://images.livemint.com/rf/Image-621x414/LiveMint/Period2/2018/08/24/Photos/Technology/z7-ksgF--621x414@LiveMint.PNG"
+                                alt="Photos"
+                            />
+                        </Grid>
+                        <Grid className="user_item_info">
+                            <h2 className="heading">Nikkon Cameron</h2>
+                            <p className="item-details">
+                                Belongs to: <span>Shutterbugs</span>
+                            </p>
+                            <p className="item-details">
+                                Issuees on: <span>30/02/2021</span>
+                            </p>
+                            <p className="item-details">
+                                Description: <span>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem</span>
+                            </p>
+                            <p className="item-details">
+                                <b>
+                                    Deadline: <span>30/02/2022</span>
+                                </b>
+                            </p>
+                            <button type="button">Request Extension</button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </React.Fragment>
     );
 }
 
-export default UserProfile;
+export default User;
