263c99be8f6f3497070062c94d036b944d46d3fd
bhawna chopra <55947219+bhawnachopra2002@users.noreply.github.com>
2021-03-06 18:02:43 +0530

    Update admin.py

--- backend/inventory/admin.py
+++ backend/inventory/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Society, Equipment
+from .models import Equipment, Equipment_issued
 
 # Register your models here.
-admin.site.register(Society)
 admin.site.register(Equipment)
+admin.site.register(Equipment_issued)
