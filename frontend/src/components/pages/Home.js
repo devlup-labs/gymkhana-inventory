699cd7e5b329e60b9600c2153be72d93c737369c
Sarthak <sarthakrajjindal44@gmail.com>
2021-03-13 01:56:16 +0530

    made changes to navbar

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import SearchIcon from '@material-ui/icons/Search';
-import IconButton from '@material-ui/core/IconButton';
+// import SearchIcon from '@material-ui/icons/Search';
+// import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -58,13 +58,13 @@ function Home() {
     const classes = useStyles();
     return (
         <div className="container home">
-            <div className="search-area">
-                <div className="texts">
+            {/* <div className="search-area"> */}
+            {/* <div className="texts">
                     <h2 id="heading">Search for Equipments</h2>
                     <p>Everything at one place</p>
-                </div>
+                </div> */}
 
-                <form>
+            {/* <form>
                     <div className="search-bar">
                         <input
                             className="bar"
@@ -81,8 +81,8 @@ function Home() {
                             <SearchIcon />
                         </IconButton>
                     </div>
-                </form>
-            </div>
+                </form> */}
+            {/* </div> */}
             <div className="sep">
                 <i>Every moment is a fresh beginning. – T.S Eliot</i>
             </div>
