13fd93bb01ae90683acc23db8c0a0c3080492a79
HardSavani <savanihard2001@gmail.com>
2021-03-23 00:06:12 +0530

    Fixed flake error

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
 
     path('login/google/', GoogleLogin.as_view(), name='google_login'),
     path('editprofile/', EditUserProfile.as_view(), name="edit_profile"),
-    path('assign-role/', AssignRole.as_view(), name="assign_role")
+    path('assign-role/', AssignRole.as_view(), name="assign_role"),
 ]
